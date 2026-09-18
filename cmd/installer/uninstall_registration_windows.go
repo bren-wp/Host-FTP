e6df4018a100363ea35ab7318660a1baa60621c6
Brendigo <info@brendigo.com>
2026-09-19 00:48:39 +0200

    Improve Windows Installed Apps metadata for integrated uninstall

--- cmd/installer/uninstall_registration_windows.go
+++ cmd/installer/uninstall_registration_windows.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/bren-wp/Host-FTP/internal/brand"
 	"github.com/bren-wp/Host-FTP/internal/platform"
@@ -17,6 +19,13 @@ func registerIntegratedUninstall(appPath, currentVersion string) error {
 	}
 
 	quoted := fmt.Sprintf("\"%s\" --uninstall", appPath)
+	info, err := os.Stat(appPath)
+	if err != nil {
+		return fmt.Errorf("installed executable size could not be read: %w", err)
+	}
+	estimatedKiB := uint32((info.Size() + 1023) / 1024)
+	installDate := time.Now().Format("20060102")
+
 	values := []struct {
 		name  string
 		value string
@@ -29,6 +38,7 @@ func registerIntegratedUninstall(appPath, currentVersion string) error {
 		{"UninstallString", quoted},
 		{installedExecutableDigestValue, digest},
 		{"URLInfoAbout", brand.Website},
+		{"InstallDate", installDate},
 	}
 	for _, item := range values {
 		if err := platform.SetRegistryString(uninstallKey, item.name, item.value); err != nil {
@@ -42,6 +52,9 @@ func registerIntegratedUninstall(appPath, currentVersion string) error {
 	// earlier installers; the surrounding registry snapshot restores it if the
 	// installation transaction later rolls back.
 	if err := platform.DeleteRegistryValue(uninstallKey, "QuietUninstallString"); err != nil {
+		return err
+	}
+	if err := platform.SetRegistryDWORD(uninstallKey, "EstimatedSize", estimatedKiB); err != nil {
 		return err
 	}
 	if err := platform.SetRegistryDWORD(uninstallKey, "NoModify", 1); err != nil {
